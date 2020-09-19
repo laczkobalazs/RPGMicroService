13867045f2875eb54ea18af1bd65ca14836c5e86
laczkobalazs <laczko.balazs89@gmail.com>
2020-09-19 16:03:47 +0200

    adds Spring diceroll module with Spring initializer

---
+++ diceroll/src/main/java/com/codecool/diceroll/DicerollApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.diceroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicerollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicerollApplication.class, args);
+	}
+
+}
