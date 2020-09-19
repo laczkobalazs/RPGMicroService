45dd128b1803f02656a31fc20def81bac0575a44
laczkobalazs <laczko.balazs89@gmail.com>
2020-09-19 16:05:30 +0200

    adds new random object to Spring application context

--- diceroll/src/main/java/com/codecool/diceroll/DicerollApplication.java
+++ diceroll/src/main/java/com/codecool/diceroll/DicerollApplication.java
@@ -2,6 +2,9 @@ package com.codecool.diceroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class DicerollApplication {
@@ -10,4 +13,8 @@ public class DicerollApplication {
 		SpringApplication.run(DicerollApplication.class, args);
 	}
 
+	@Bean
+	public Random random() {
+		return new Random();
+	}
 }
