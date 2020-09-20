948e6f186026035b05952150bf6fdbd745607c5e
laczkobalazs <laczko.balazs89@gmail.com>
2020-09-20 15:14:39 +0200

    adds service registry module

--- diceroll/src/main/java/com/codecool/diceroll/DicerollApplication.java
+++ diceroll/src/main/java/com/codecool/diceroll/DicerollApplication.java
@@ -3,10 +3,16 @@ package com.codecool.diceroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Random;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DicerollApplication {
 
 	public static void main(String[] args) {
@@ -16,5 +22,14 @@ public class DicerollApplication {
 	@Bean
 	public Random random() {
 		return new Random();
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.ant("/roll/**"))
+				.build();
 	}
 }
--- diceroll/src/main/java/com/codecool/diceroll/controller/DiceRollController.java
+++ diceroll/src/main/java/com/codecool/diceroll/controller/DiceRollController.java
@@ -1,8 +1,12 @@
 package com.codecool.diceroll.controller;
 
 import com.codecool.diceroll.service.DiceRollService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +18,29 @@ public class DiceRollController {
     @Autowired
     private DiceRollService service;
 
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/d6")
+    public Roll rollD6() {
+        return new Roll(
+                service.rollD6(),
+                "D6",
+                env.getProperty("server.port")
+        );
+    }
+
+    @GetMapping("/d20")
+    public Roll rollD20() {
+        return new Roll(
+                service.rollD20(),
+                "D20",
+                env.getProperty("server.port")
+        );
+    }
+
+    @Data
+    @AllArgsConstructor
     public class Roll {
         private int result;
         private String dice;
